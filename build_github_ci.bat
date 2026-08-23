8a837f0302e419ffe1015b13f7c731253dd9cb44
Dong Yuwei <newdongyuwei@gmail.com>
2026-08-23 17:06:06 +0800

    Let cmake auto-detect Visual Studio on CI (VS2026 image)

--- build_github_ci.bat
+++ build_github_ci.bat
@@ -1,6 +1,6 @@
 set CMAKE_POLICY_VERSION_MINIMUM=3.5
-cmake . -Bbuild -G "Visual Studio 17 2022" -A Win32
+cmake . -Bbuild -A Win32
 cmake --build build --config Release
 
-cmake . -Bbuild64 -G "Visual Studio 17 2022"  -A x64
+cmake . -Bbuild64 -A x64
 cmake --build build64 --config Release --target PIMETextService
