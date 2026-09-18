99afbee917cb6a42c5605f6f665f4004d43d3cb3
Dong Yuwei <newdongyuwei@gmail.com>
2026-09-18 17:40:08 +0800

    Parallelize build: msbuild /m and /MP compiler flags

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 rem allow old cmake_minimum_required() declarations to work with CMake 4.x
 set CMAKE_POLICY_VERSION_MINIMUM=3.5
 rem no -G: let cmake auto-detect the installed Visual Studio (VS2019 on AppVeyor, VS2026 on GitHub CI)
+rem -j: let msbuild build projects in parallel; /MP (see cmake/*_flag_overrides.cmake) parallelizes files within a project
 cmake . -Bbuild -A Win32
-cmake --build build --config Release
+cmake --build build --config Release --parallel
 
 cmake . -Bbuild64 -A x64
-cmake --build build64 --config Release --target PIMETextService
+cmake --build build64 --config Release --target PIMETextService --parallel
